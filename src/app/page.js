16cf448a42951eeded0d96fe32d6ad1a01bf6162
Ghedyvan <gvbs@ic.ufal.br>
2023-11-08 05:08:35 -0300

    att

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
-import { Background } from "@/components/mylinks/Background";
-import { MyLinksBody } from "@/components/mylinks/MyLinksBody";
-import { MyLinksHeader } from "@/components/mylinks/MyLinksHeader";
+import  Background  from "@/components/mylinks/Background";
+import  MyLinksBody  from "@/components/mylinks/MyLinksBody";
+import  MyLinksHeader  from "@/components/mylinks/MyLinksHeader";
 
 export default function Home() {
   return (
